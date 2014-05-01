d06d7061e25bffd25777ce71843da54f88fa7528
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2014-04-30 23:35:57 -0700

    Add README, LICENSE, github banner, help on page

---
+++ README.md
@@ -0,0 +1,43 @@
+# Chrome Draw
+
+Welcome to chrome draw.  This is a drawing app built to be turned in to a chrome extension to mark up any web page.
+
+## Demo:
+
+Requires WebGL for display, Leap Skeletal Beta for interaction:
+
+[leapmotion.github.io/chrome-draw/](http://leapmotion.github.io/chrome-draw/)
+
+## Building
+
+It is currently implemented in coffee script.  To compile your own changes, make sure you have grunt installed
+(`npm install grunt`), and then simply run
+
+```bash
+> grunt watch
+```
+
+As well as transpiling the coffeescript in to `main.js`, this will also run a livereload server.  With the right
+[chrome extension](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?hl=en), the page will reload for you when files are changed.
+
+## Known issues and road map
+
+ - Hand splay should be measuring how stretched-out fingers are, but actually measures how together they are
+ - Sometimes some brush strokes are left behind when clearing the canvas
+ - Leap-playback can sometimes speed up
+
+**Roadmap**:
+
+ - Wire up the color choosers on the top right
+ - Allow click interaction, to go between moving hand Up/Down for hue and Up/Down/Left/Right for saturation
+ - Build in to chrome extension, for use on any page
+ - Add "new canvas" and "take screenshot" buttons
+ - Add share buttons
+
+
+## Components
+
+ - The [Rigged Hand](https://github.com/leapmotion/leapjs-rigged-hand) - 3d hand on screen
+ - [Leapjs-playback](https://github.com/leapmotion/leapjs-playback) - plays back pre-recorded motion
+ - *leap.relative-motion* This beta plugin allows motion relative to the position of the hand when last recalibrated
+ - *leap.hand-splay* This beta plugin measures how flat and stretched-out a hand is.
--- gruntfile.js
+++ gruntfile.js
@@ -28,9 +28,36 @@ module.exports = function (grunt) {
           spawn: false
         }
       }
+    },
+    usebanner: {
+      build: {
+        src: ["javascripts/main.js"],
+        options: {
+          banner: '/*                                                                                       ' +
+                '\n * Chrome Draw - v<%= pkg.version %> - <%= grunt.template.today(\"yyyy-mm-dd\") %>' +
+                '\n * http://github.com/leapmotion/leapjs-rigged-hand/                                      ' +
+                '\n *                                                                                       ' +
+                '\n * Copyright <%= grunt.template.today(\"yyyy\") %> LeapMotion, Inc                       ' +
+                '\n *                                                                                       ' +
+                '\n * Licensed under the Apache License, Version 2.0 (the "License");                       ' +
+                '\n * you may not use this file except in compliance with the License.                      ' +
+                '\n * You may obtain a copy of the License at                                               ' +
+                '\n *                                                                                       ' +
+                '\n *     http://www.apache.org/licenses/LICENSE-2.0                                        ' +
+                '\n *                                                                                       ' +
+                '\n * Unless required by applicable law or agreed to in writing, software                   ' +
+                '\n * distributed under the License is distributed on an "AS IS" BASIS,                     ' +
+                '\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.              ' +
+                '\n * See the License for the specific language governing permissions and                   ' +
+                '\n * limitations under the License.                                                        ' +
+                '\n *                                                                                       ' +
+                '\n */                                                                                      ' +
+                '\n'
+        }
+      }
     }
 
   });
   require('load-grunt-tasks')(grunt);
-  grunt.registerTask('default', ['coffee', 'connect', 'watch'])
+  grunt.registerTask('default', ['coffee', 'usebanner', 'connect', 'watch'])
 };                            
